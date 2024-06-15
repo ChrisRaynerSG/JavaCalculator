dfb0473f4ea9cd3dafecebbe5b37ba421efa2638
Chris Rayner <crayner@spartaglobal.com>
2024-06-15 18:18:34 +0100

    Created basic GUI for calculator application

--- src/CalculatorWindow.java
+++ src/CalculatorWindow.java
@@ -3,57 +3,90 @@ import java.awt.*;
 
 public class CalculatorWindow{
     CalculatorWindow(){
-        JFrame calculatorWindow = new JFrame();
+        JFrame calculatorWindow = new JFrame("Calculator");
         calculatorWindow.setSize(300,400);
         calculatorWindow.setResizable(false);
-
         calculatorWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JPanel mainPanel = new JPanel();
-
-        JPanel buttonGrid = new JPanel(new GridBagLayout());
-
+        JPanel middleNumberGridPanel = new JPanel();
+        JPanel equalsButtonPanel = new JPanel();
+        JTextArea numberOutput = new JTextArea();
+        numberOutput.setPreferredSize(new Dimension(300,60));
+        GridLayout middleNumberGrid = new GridLayout(4,4);
+        middleNumberGridPanel.setLayout(middleNumberGrid);
 
         JButton zeroButton = new JButton("0");
-        JButton decimalPoint = new JButton(".");
-        JButton equalsButton = new JButton("=");
         JButton oneButton = new JButton("1");
         JButton twoButton = new JButton("2");
         JButton threeButton = new JButton("3");
-        JButton plusButton = new JButton("+");
         JButton fourButton = new JButton("4");
         JButton fiveButton = new JButton("5");
         JButton sixButton = new JButton("6");
-        JButton minusButton = new JButton("-");
         JButton sevenButton = new JButton("7");
         JButton eightButton = new JButton("8");
         JButton nineButton = new JButton("9");
+        JButton decimalPointButton = new JButton(".");
+        JButton equalsButton = new JButton("=");
+        JButton plusButton = new JButton("+");
+        JButton minusButton = new JButton("-");
         JButton multiplyButton = new JButton("x");
+        JButton divideButton = new JButton("÷");
+        JButton clearButton = new JButton("C");
+
+        equalsButton.setPreferredSize(new Dimension(300,40));
 
 
-        buttonGrid.add(zeroButton);
-        buttonGrid.add(oneButton);
-        buttonGrid.add(twoButton);
-        buttonGrid.add(threeButton);
-        buttonGrid.add(fourButton);
-        buttonGrid.add(fiveButton);
-        buttonGrid.add(sixButton);
-        buttonGrid.add(sevenButton);
-        buttonGrid.add(eightButton);
-        buttonGrid.add(nineButton);
+        // Number Output
+        // 7 8 9 ÷
+        // 4 5 6 x
+        // 1 2 3 -
+        // C 0 . +
+        // =
 
-        mainPanel.add(buttonGrid);
-        calculatorWindow.add(mainPanel);
+        //Add 4x4 grid buttons to grid layout
+        middleNumberGridPanel.add(sevenButton);
+        middleNumberGridPanel.add(eightButton);
+        middleNumberGridPanel.add(nineButton);
+        middleNumberGridPanel.add(divideButton);
+        middleNumberGridPanel.add(fourButton);
+        middleNumberGridPanel.add(fiveButton);
+        middleNumberGridPanel.add(sixButton);
+        middleNumberGridPanel.add(multiplyButton);
+        middleNumberGridPanel.add(oneButton);
+        middleNumberGridPanel.add(twoButton);
+        middleNumberGridPanel.add(threeButton);
+        middleNumberGridPanel.add(minusButton);
+        middleNumberGridPanel.add(clearButton);
+        middleNumberGridPanel.add(zeroButton);
+        middleNumberGridPanel.add(decimalPointButton);
+        middleNumberGridPanel.add(plusButton);
+
+
+
+
+
+//        middleNumberGrid.addLayoutComponent("0",zeroButton);
+//        middleNumberGrid.addLayoutComponent(".",decimalPointButton);
+//        middleNumberGrid.addLayoutComponent("+",plusButton);
+//        middleNumberGrid.addLayoutComponent("1",oneButton);
+//        middleNumberGrid.addLayoutComponent("2",twoButton);
+//        middleNumberGrid.addLayoutComponent("3",threeButton);
+//        middleNumberGrid.addLayoutComponent("-",minusButton);
+//        middleNumberGrid.addLayoutComponent("4",fourButton);
+//        middleNumberGrid.addLayoutComponent("5",fiveButton);
+//        middleNumberGrid.addLayoutComponent("6",sixButton);
+//        middleNumberGrid.addLayoutComponent("x",multiplyButton);
+//        middleNumberGrid.addLayoutComponent("7",sevenButton);
+//        middleNumberGrid.addLayoutComponent("8",eightButton);
+//        middleNumberGrid.addLayoutComponent("9",nineButton);
+//        middleNumberGrid.addLayoutComponent("÷",divideButton);
+
+        equalsButtonPanel.add(equalsButton);
+        calculatorWindow.add(numberOutput, BorderLayout.NORTH);
+        calculatorWindow.add(middleNumberGridPanel, BorderLayout.CENTER);
+        calculatorWindow.add(equalsButton,BorderLayout.SOUTH);
+
         calculatorWindow.setVisible(true);
-
-
-
-
-
-
-
-
-
 
     }
 
